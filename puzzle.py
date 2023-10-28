04a264df017f6c47f38487bdb110c34710762efb
Marcin <marcan.grab@gmail.com>
2023-10-28 17:32:54 +0200

    finished 0,1,2 puzzle working on 3

--- puzzle.py
+++ puzzle.py
@@ -12,34 +12,66 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    Not(And(AKnight, AKnave)),Or(AKnave,AKnight),Implication(AKnight,AKnight), Implication(AKnave,Not(AKnave)), Implication(And(AKnight,AKnave),AKnave)
+    Not(And(AKnight, AKnave)),
+    Or(AKnave, AKnight),
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave))),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    Not(And(AKnight, AKnave)),Or(AKnave,AKnight),Implication(AKnight,AKnight), Implication(AKnave,Not(AKnave)),
+    Not(And(AKnight, AKnave)),
+    Or(AKnave, AKnight),
+    Not(And(BKnight, BKnave)),
+    Or(BKnave, BKnight),
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    Not(And(AKnight, AKnave)),Or(AKnave,AKnight),Implication(AKnight,AKnight), Implication(AKnave,Not(AKnave)),
+    Not(And(AKnight, AKnave)),
+    Or(AKnave, AKnight),
+    Not(And(BKnight, BKnave)),
+    Or(BKnave, BKnight),
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
 )
 
 # Puzzle 3
-# A says either "I am a knight." or "I am a knave.", but you don't know which.
-# B says "A said 'I am a knave'."
-# B says "C is a knave."
-# C says "A is a knight."
+# A says either "I am a knight." or "I am a knave.", but you don't know which./knight
+# B says "A said 'I am a knave'."/knave
+# B says "C is a knave."/knave
+# C says "A is a knight."/knight
 knowledge3 = And(
-    Not(And(AKnight, AKnave)),Or(AKnave,AKnight),Implication(AKnight,AKnight), Implication(AKnave,Not(AKnave)),
+    Not(And(AKnight, AKnave)),
+    Or(AKnave, AKnight),
+    Not(And(BKnight, BKnave)),
+    Or(BKnave, BKnight),
+    Not(And(CKnight, CKnave)),
+    Or(CKnave, CKnight),
+    Or(Implication(AKnight, AKnight), Implication(AKnight, AKnave)),
+    Or(Implication(AKnave, Not(AKnight)), Implication(AKnave, Not(AKnave))),
+    Implication(
+        BKnight, Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))
+    ),
+    Implication(
+        BKnave,
+        Or(Not(Implication(AKnight, AKnave)), Not(Implication(AKnave, Not(AKnave)))),
+    ),
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight)),
 )
 
 print(knowledge0.formula())
-
 
 
 def main():
@@ -48,7 +80,7 @@ def main():
         ("Puzzle 0", knowledge0),
         ("Puzzle 1", knowledge1),
         ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        ("Puzzle 3", knowledge3),
     ]
     for puzzle, knowledge in puzzles:
         print(puzzle)
